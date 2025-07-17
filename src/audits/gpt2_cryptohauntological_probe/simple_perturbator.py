4f691a26bf32318d3e06523089ebaa16533371e8
Ilya Mozerov <ilyailiilia@gmail.com>
2025-07-17 13:06:40 +0200

    fix: Add uni_mirror rule to SimplePerturbator for enhanced text transformation

--- src/audits/gpt2_cryptohauntological_probe/simple_perturbator.py
+++ src/audits/gpt2_cryptohauntological_probe/simple_perturbator.py
@@ -21,6 +21,7 @@ class SimplePerturbator(BasePerturbator):
             "reverse_string": self._reverse_string,
             "zero_width_space": self._zero_width_space,
             "caesar_cipher": self._caesar_cipher,
+            "uni_mirror": self._uni_mirror,
             "y_z_swap": self._y_z_swap # New rule for Y<->Z swap
         }
 
@@ -40,6 +41,22 @@ class SimplePerturbator(BasePerturbator):
 
     def _character_doubling(self, text):
         return "".join([char * 2 for char in text])
+
+    def _uni_mirror(self, text):
+        ret_val = text.replace('o', '#TEMP#').replace('o', 'о').replace('#TEMP#', 'о')
+        ret_val = text.replace('O', '#TEMP#').replace('O', 'О').replace('#TEMP#', 'О')
+        ret_val = text.replace('c', '#TEMP#').replace('c', 'с').replace('#TEMP#', 'с')
+        ret_val = text.replace('C', '#TEMP#').replace('C', 'С').replace('#TEMP#', 'С')
+        ret_val = text.replace('e', '#TEMP#').replace('e', 'е').replace('#TEMP#', 'е')
+        ret_val = text.replace('E', '#TEMP#').replace('E', 'Е').replace('#TEMP#', 'Е')
+        ret_val = text.replace('a', '#TEMP#').replace('a', 'а').replace('#TEMP#', 'а')
+        ret_val = text.replace('A', '#TEMP#').replace('A', 'А').replace('#TEMP#', 'А')
+        ret_val = text.replace('p', '#TEMP#').replace('p', 'р').replace('#TEMP#', 'р')
+        ret_val = text.replace('P', '#TEMP#').replace('P', 'Р').replace('#TEMP#', 'Р')
+        ret_val = text.replace('x', '#TEMP#').replace('x', 'х').replace('#TEMP#', 'х')
+        ret_val = text.replace('X', '#TEMP#').replace('X', 'Х').replace('#TEMP#', 'Х')
+        return ret_val
+
 
     def _reverse_string(self, text):
         return text[::-1]
